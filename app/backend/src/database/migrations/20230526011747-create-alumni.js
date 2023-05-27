aa7a364a639a5cf5c6aedcee9b1b659db2667203
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-26 21:10:29 -0300

    Adiciona create-alumni.js em migrations

---
+++ app/backend/src/database/migrations/20230526011747-create-alumni.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('alumnis', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      alumni_photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      alumni_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      alumni_resume: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('alumnis');
+  }
+};
