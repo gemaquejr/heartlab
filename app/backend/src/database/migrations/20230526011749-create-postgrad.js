0766e7274abc8c3bcb7e4b9193129f13d209db4b
gemaquejr <gemaquejr@hotmail.com>
2024-01-13 20:01:15 -0300

    Adiciona migration de postgradStudents
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ app/backend/src/database/migrations/20230526011749-create-postgrad.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('undergradStudents', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      experience: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      postgrad_photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      postgrad_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      nationality: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      curriculum: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      advisor: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      postgrad_resume: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      hobbies: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      education: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('postgradStudents');
+  }
+};
