e6c797e9a4a50723bf24bad1bc95a9c05a9796dd
gemaquejr <gemaquejr@hotmail.com>
2024-01-13 18:55:29 -0300

    Fix model de undergrad students scientific iniciation
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/backend/src/database/models/undergradStudents.ts
+++ app/backend/src/database/models/undergradStudents.ts
@@ -3,9 +3,18 @@ import db from '.';
 
 class UndergradStudents extends Model {
   public id!: number;
+  public experience!: string;
   public undergradPhoto!: string;
   public undergradName!: string;
+  public nationality!: string;
+  public age!: string;
+  public email!: string;
+  public phone!: string;
+  public curriculum!: string;
+  public advisor!: string;
   public undergradResume!: string;
+  public hobbies!: string;
+  public education!: string;
 
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
@@ -13,24 +22,60 @@ class UndergradStudents extends Model {
 
 UndergradStudents.init({
   id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
-    },
-    undergrad_photo: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    undergrad_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    undergrad_resume: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  }, {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  experience: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  alumni_photo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  alumni_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  nationality: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  curriculum: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  advisor: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  alumni_resume: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  hobbies: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  education: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
     underscored: true,
     sequelize: db,
     modelName: 'undergradStudents',
