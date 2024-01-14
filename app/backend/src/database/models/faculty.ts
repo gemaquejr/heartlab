94ec30565ee37592ea0b3fdc4bafdb48e1c31484
gemaquejr <gemaquejr@hotmail.com>
2024-01-14 14:14:23 -0300

    Adiciona model de Faculty
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ app/backend/src/database/models/faculty.ts
@@ -0,0 +1,55 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Faculty extends Model {
+  public id!: number;
+  public facultyPhoto!: string;
+  public facultyName!: string;
+  public email!: string;
+  public phone!: string;
+  public curriculum!: string;
+  public facultyResume!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Faculty.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  faculty_photo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  faculty_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  curriculum: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  faculty_resume: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+    underscored: true,
+    sequelize: db,
+    modelName: 'faculty',
+    timestamps: false,
+  });
+
+export default Faculty;
