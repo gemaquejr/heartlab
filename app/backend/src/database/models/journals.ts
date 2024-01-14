9319194d407035aea0318adfb489fcd6ad3d6679
gemaquejr <gemaquejr@hotmail.com>
2024-01-14 17:22:43 -0300

    Adiciona model de Journals
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ app/backend/src/database/models/journals.ts
@@ -0,0 +1,35 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Journals extends Model {
+  public id!: number;
+  public age!: string;
+  public conference!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Journals.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  conference: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+    underscored: true,
+    sequelize: db,
+    modelName: 'journals',
+    timestamps: false,
+  });
+
+export default Journals;
