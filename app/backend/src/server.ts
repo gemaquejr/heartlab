9b7678f0b25df333787fc2ae59162dc8dc1a3e93
gemaquejr <gemaquejr@hotmail.com>
2024-01-16 22:15:36 -0300

    Adiciona route de alumni
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use('/alumnis', routes.AlumniRouter);
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port: ${PORT}`)
