2d3f60f4a20bc0875c8d0f052c663c5f5c063f27
gemaquejr <gemaquejr@hotmail.com>
2024-01-16 22:13:22 -0300

    Fix database.ts
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,10 +2,10 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  password: process.env.DB_PASS || '12345678',
   database: process.env.DB_NAME || 'HEARTLAB',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
 };
 
