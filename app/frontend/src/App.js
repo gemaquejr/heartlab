df3a642117abf184db90565ef3913864dd48b502
gemaquejr <gemaquejr@hotmail.com>
2024-01-27 20:40:48 -0300

    Adiciona base de header
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+
 import './App.css';
+import Header from './components/Header/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Heartlab</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <h1>Heartlab</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ app/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './Header.module.css'
+
+export default function Header() {
+    return (
+        <header className={styles.teste}>
+            <div className={styles.teste}>
+                <a href="/"> </a>  
+            </div>
+            <div className={styles.teste}>
+                <a href="/"> </a>  
+            </div>
+        </header>
+    )
+}
