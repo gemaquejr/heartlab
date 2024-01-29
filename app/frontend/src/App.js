484ccca2077b8078a20071e9c16eeb0a4f87a1ff
gemaquejr <gemaquejr@hotmail.com>
2024-01-29 17:48:09 -0300

    Adiciona page Home
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import './App.css';
 import Header from './components/Header/Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-        <h1>Heartlab</h1>
+        <Home />
       </div>
     </BrowserRouter>
   );
